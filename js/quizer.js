1eb0ac45c52bd67bde04f09126966b061c8f6348
Vitinary <153402650+Vitinary@users.noreply.github.com>
2024-02-25 07:26:01 +0300

    Add files via upload

--- js/quizer.js
+++ js/quizer.js
@@ -146,6 +146,7 @@ function toggleGameButton(){
 let lang_letter;
 
 function learn(){
+	hide_navi_icons();
 	if(withoutAnswers){
 		$('.without_answers').show();
 	} else {
@@ -706,10 +707,13 @@ let music = [
 let songs_to_map;
 let mapping_result;
 function map_songs(){
+	back = back_to_current_pack;
 	$('.package').hide();
 	$('#mirror').hide();
 	$('#map').hide();
-	$('#mapping').show();
+	$('#package_content').hide();
+	$('#mapping_content').show();
+	toggleLearn();
 	for(var j=0; j < music.length; j++){
 		music[j].arr = generateSongIdsWithPrefix(music[j].arr, music[j].lang, 
 												music[j].year, music[j].type);
@@ -891,10 +895,8 @@ function show_packages(num){
 }
 
 function package_num(num){
-	hide_navi_icons();
 	$('#current_pack').show();
 	$('#current_pack').attr('src', $('#package_' + num).attr('src'));
-	back = back_to_packages;
 	$('.package').hide();
 	setPathsByPack(num);
 	showGroupNames();
@@ -1007,14 +1009,6 @@ function generateSongIds(arr){
 	return arr;
 }
 
-function back_to_packages(){
-	$('#back').hide();
-	$('#current_pack').hide();
-	$('#package_content').hide();
-	toggleLearn();
-	setup();
-}
-
 let back;
 let expressMode = false;
 let generateSongs;
@@ -1026,7 +1020,6 @@ function setup(){
 	lang = 'ru';
 	year = '2000';
 	artist_type = 'm';
-	back = back_to_packages;
 	modeToggle = toggleArtist;
 	setMedia = setAudio;
 	rightAnswer = rightAnswer_RU;
@@ -1034,4 +1027,29 @@ function setup(){
 	package_names = ru_2000_m_icon;
 	show_packages(package_names.length);
 	document.body.scrollTop = document.documentElement.scrollTop = 0;
+	useUrlParam();
+}
+
+let pack_num;
+let year_url = 'https://sunquiz.netlify.app/2000';
+
+function useUrlParam() {
+	var url_string = window.location.href; 
+	var url = new URL(url_string);
+	pack_num = url.searchParams.get("pack");
+	if(pack_num){
+		package_num(pack_num);
+	}
+	back = back_to_browser;
+}
+
+function back_to_browser(){
+	window.location.href = year_url;
+}
+
+function back_to_current_pack(){
+	back = back_to_browser;
+	$('#mapping_content').hide();
+	$('#map').show();
+	package_num(pack_num);
 }
